07bcaa84691ceaeec968dbb4c32757c9f50b6625
ESTANISLAO ALIAGA KUZYK <esalku@floridauniversitaria.es>
2022-12-14 12:00:29 +0100

    Creamos conexion estatica como metodo y encriptamos la contraseña en metodo

--- AEV2Mongo/src/acts/Modelo.java
+++ AEV2Mongo/src/acts/Modelo.java
@@ -15,51 +15,70 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class Modelo {
 
-	public static void main(String[] args) throws IOException {
-		
-		String ipString;
-		int port;
-		String bbddString;
-		String collectionBooks;
-		String collectionUsers;
-		
-		
+	static String ipString;
+	static int port;
+	static String bbddString;
+	static String collectionBooks;
+	static String collectionUsers;
+	static MongoClient mongoClient;
+
+	public static String generarHash(String password) {
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+		byte[] hash = md.digest(password.getBytes());
+		StringBuffer sb = new StringBuffer();
+
+		for (byte b : hash) {
+			sb.append(String.format("%02x", b));
+		}
+		System.out.println(sb.toString());
+		return sb.toString();
+	}
+
+	public static void iniciarConexion() throws IOException {
 		File f1 = new File("conexion.txt");
-		
+
 		FileReader fReader = new FileReader(f1);
 		BufferedReader br = new BufferedReader(fReader);
 		String linea = br.readLine();
-		String cont=linea;
-		
+		String cont = linea;
+
 		do {
 			linea = br.readLine();
-			cont+=linea;
-		} while (linea!=null);
+			cont += linea;
+		} while (linea != null);
 
-		
 		JSONObject obj = new JSONObject(cont);
 
-		ipString=obj.getString("IP");
-		port=obj.getInt("port");
-		bbddString=obj.getString("bbdd");
-		collectionBooks=obj.getString("coleccio1");
-		collectionUsers=obj.getString("coleccio2");
-		System.out.println(ipString);
-		System.out.println(port);
-		System.out.println(bbddString);
-		System.out.println(collectionBooks);
-		
-		MongoClient mongoClient = new MongoClient(ipString, port);
-		MongoDatabase database = mongoClient.getDatabase(bbddString);
-		MongoCollection<Document>  coleccionBooks = database.getCollection(collectionBooks);
-//		MongoCollection<Document>  coleccionUsers = database.getCollection(collectionUsers);
+		ipString = obj.getString("IP");
+		port = obj.getInt("port");
+		bbddString = obj.getString("bbdd");
+		collectionBooks = obj.getString("coleccio1");
+		collectionUsers = obj.getString("coleccio2");
 
-		
+		mongoClient = new MongoClient(ipString, port);
+		MongoDatabase database = mongoClient.getDatabase(bbddString);
+		MongoCollection<Document> coleccionBooks = database.getCollection(collectionBooks);
+		MongoCollection<Document> coleccionUsers = database.getCollection(collectionUsers);
+
 		System.out.println("ye bon dia");
-		
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		generarHash("elowo");
+
 		mongoClient.close();
 
 	}
